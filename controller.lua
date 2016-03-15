ef6be9970fe99c5a629a99cfb782345341701583
Deviani <jeroen.claes12@gmail.com>
2016-03-15 15:14:26 +0100

    Update controller.lua
    Values for in and output tuned by maccydave

--- controller.lua
+++ controller.lua
@@ -1,6 +1,3 @@
-local Increment = 1000
-local Threshold = 300000000
-
 local function find( _type )
     for _, name in ipairs( peripheral.getNames() ) do
         if peripheral.getType( name ) == _type then
@@ -9,38 +6,75 @@ local function find( _type )
     end
 end
 
-reactor = peripheral.wrap(find("draconic_reactor"))
-fluxGate = peripheral.wrap(find("flux_gate"))
-mon = peripheral.wrap(find("monitor"))
+function initializeController()
+	--set up reactor devices
+	reactor = peripheral.wrap(find("draconic_reactor"))
+	inputFluxGate = peripheral.wrap("flux_gate_0")
+	outputFluxGate = peripheral.wrap("flux_gate_1")
+	mon = peripheral.wrap(find("monitor"))
+	
+	--placeholders need to be tweaked by experimenting with the reactor
+	timestep = 50
+	integral = 0
+	
+	
+	--input controller parameters
+	maxInputValue = 500000
+	contStrTarget = 50000000
+	inputKP = 1
+	inputKI = 0.001
+	inputKD = 1
+	inputFluxGate.setSignalLowFlow(10000)
+	
+	--initilize variables
+	inputIntegral = 0
+	inputDerivate = 0
+	outputIntegral = 0
+	outputDerivate = 0
+	preInputError = 0
+	preOutputError = 0
+	outputValue = 0
+	
+	--output controller parameters
+	maxOutputValue = 600000
+	eSatTarget=300000000
+	outputKP = 1
+	outputKI = 0.001
+	outputKD = 1
+	--TODO use fixed value until input parameters are stabilized.
+	--outputFluxGate.setSignalLowFlow(10000)
+end
 
 function cls()
+	--clear the monitor
 	mon.clear()
 	mon.setCursorPos(1,1)
 end
 
 function line(y)
+	--go to specified line on the monitor
 	mon.setCursorPos(1,y)
 end
 
 function getInfo()
+	--update reactor internal values
 	rInfo = reactor.getReactorInfo()
 	
 	tmp = rInfo["temperature"]
 	eSat = rInfo["energySaturation"]
 	contStr = rInfo["fieldStrength"]
+	contDrain = rInfo["fieldDrainRate"]
 	genRate = rInfo["generationRate"]
 	fConv = rInfo["fuelConversion"]
 	fConvR = rInfo["fuelConversionRate"]
 end
 
 function formatDisplay()
+	--update the display information
 	rfCharge = eSat * 0.0000001
 	fieldStr = contStr * 0.000001
 	rfDisplay = genRate / 1000
 	fuel = (fConv / 10368) *100
-	if fConvR == nil then
-		fConvR = "Not Running"
-	end
 	
 	line(1)
 	mon.write("Reactor temperature = " .. tmp)
@@ -53,89 +87,118 @@ function formatDisplay()
 	line(6)
 	mon.write("RF production = " .. rfDisplay .. " RF/t")
 	line(7)
-	Gate()
+	mon.write("Containtment Strain = " .. contDrain .. " RF/t")
+	line(8)
+	mon.write("Net = " ..(genRate - contDrain) .. " RF/t")
+	line(10)
 end
 
-function Gate()
-	lowFlow = fluxGate.getSignalLowFlow()
-	
-	if eSat > Threshold then
-		if lowFlow >= (700000 - Increment) then
-			lowFlow = (700000 - Increment)
-		end
-		
-		lowFlow = lowFlow + Increment
-		
-	else
-		if lowFlow < Increment then
-			lowFlow = Increment
-		end
-		
-		lowFlow = lowFlow - Increment
+function regulateInput()
+	-- Reactor startup
+	if rInfo["status"] == "charging" then
+		inputFluxGate.setSignalLowFlow(300000)
+		return
+	elseif rInfo["status"] == "charged" then
+		reactor.activateReactor()
 	end
 	
-	mon.write("Flux Gate set to " .. lowFlow .. " RF/t")
-	line(9)
-	fluxGate.setSignalLowFlow(lowFlow)
+	--sets the value of the input gate of the reactor.
+	--call this function each time step to regulate the input level of the reactor
+	inputError = contStrTarget - contStr
+
+	inputIntegral = inputIntegral + (inputError * timestep)
+	inputDerivate = (inputError - preInputError) / timestep
+
+	--set the actual value on the flow gate
+	inputValue = (inputKP * inputError) + (inputKI * inputIntegral) + (inputKD * inputDerivate)
+	print("Input: " .. inputValue)
+	
+	if inputValue < 0 then
+		inputValue = 0
+	elseif inputValue > maxInputValue then
+		inputValue = maxInputValue
+	end
+	
+	inputFluxGate.setSignalLowFlow(inputValue)
+	
+	-- to ease it down, so it doesn't take too long to get back to 0 input value
+	--if contStrTarget > 5000000 and inputValue > 0 then
+		--contStrTarget = contStrTarget - 100000
+	--end
+	
+	contStrTarget = 5000000
+		
+	--save the error for next cycle
+	preInputError = inputError
+end
+	
+function regulateOutput()
+	-- Reactor Heat up
+	if tmp < 7500 then
+		outputFluxGate.setSignalLowFlow(maxOutputValue)
+		return
+	end
+
+	--sets the value of the output gate of the reactor.
+	--call this function each time step to regulate the output level of the reactor
+	outputError = eSatTarget - eSat 
+	outputIntegral = outputIntegral + (outputError * timestep)
+	outputDerivate = (outputError - preOutputError) / timestep
+	
+	--set the actual value on the flow gate
+	outputValue = ((outputKP * outputError) + (outputKI * outputIntegral) + (outputKD * outputDerivate)) * -1
+	
+	print("Output: " .. outputValue)
+	if outputValue < 0 then
+		outputValue = 0
+	elseif outputValue > maxOutputValue then
+		outputValue = maxOutputValue
+	end
+	
+	outputFluxGate.setSignalLowFlow(outputValue)
+	
+	--save the error for next cycle
+	preOutputError = outputError
 end
 
 function safety()
-	if tmp > 7777 then
+	eText = "Reactor Running"
+	--checks all safety constraints of the controller
+	if tmp > 8000 then
 		eText = "Reactor too hot"
 		reactor.stopReactor()
 		stopped = true
 	end
 	
-	if fConv >= 75 then
+	if fConv >= 95 then
 		eText = "Reactor needs to refuel"
 		reactor.stopReactor()
 		stopped = true
 	end
 	
-	if tmp < 2000 then
+	if tmp < 2000 and fConv <= 95 then
 		reactor.chargeReactor()
 		eText = "Charging reactor"
-		if contStr >= 50000000 and stopped then
-			eText = "Activating reactor"
-			reactor.activateReactor()
-			stopped = false
-		end
-	else
-		if contStr <= 15000000 then
-			eText = "Containtment field too weak"
-			reactor.stopReactor()
-			stopped = true
-			contLow = true
-		end
-		
-		if contStr >= 20000000 and stopped and contLow then
-			reactor.activateReactor()
-			eText = "Reactor activated (containment)"
-			stopped = false
-			contLow = false
-		end
-	end
-	
-	if eSat <= 15000000 then
-		reactor.stopReactor()
-		eText = "Energy saturation too low"
-		
-		stopped = true
-	end
-	
-	if eSat >= 20000000 and stopped then
-		reactor.activateReactor()
-		eText = "Reactor activated"
-		stopped = false
 	end
 	
 	mon.write(eText)
 end
 
-while true do
-	getInfo()
-	formatDisplay()
-	safety()
-	sleep(0.1)
-	cls()
+function run()
+	initializeController()
+	while true do
+		-- debug
+		term.clear()
+		term.setCursorPos(1,1)
+		
+		getInfo()
+		regulateInput()
+		regulateOutput() 
+		formatDisplay()
+		safety()
+		sleep(timestep / 1000)
+		cls()
+	end
 end
+
+run()
